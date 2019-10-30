11440d075591ac24d24628f531907fcdd46ebf83
Tatsu015 <tatsumi.soichiro.com>
2019-10-30 22:39:16 +0900

    modify node and connection undo/redo

--- src/core/controller/command/NodeRemoveCommand.cpp
+++ src/core/controller/command/NodeRemoveCommand.cpp
@@ -6,68 +6,83 @@
 #include "Scene.h"
 #include "Sheet.h"
 
+NodeRemoveCommand::ConnectionInfo::ConnectionInfo(Connection* connection, Port* startPort, Port* endPort)
+    : m_connection(connection), m_startPort(startPort), m_endPort(endPort) {
+}
+
+NodeRemoveCommand::ConnectionInfo::~ConnectionInfo() {
+}
+
+void NodeRemoveCommand::ConnectionInfo::reconnect() {
+  if (m_endPort) {
+    m_endPort->addConnection(m_connection);
+    m_connection->setEndPort(m_endPort);
+  } else if (m_startPort) {
+    m_startPort->addConnection(m_connection);
+    m_connection->setStartPort(m_startPort);
+  }
+}
+
 NodeRemoveCommand::NodeRemoveCommand(Scene* scene, Sheet* sheet, QList<AbstractNode*> nodes)
     : QUndoCommand(), m_scene(scene), m_sheet(sheet) {
-  QList<Connection*> checkedConnections;
-
-  foreach (AbstractNode* node, nodes) {
-    NodeRemoveInfo* nodeRemoveInfo = new NodeRemoveInfo();
-    nodeRemoveInfo->m_node = node;
-    foreach (Port* port, node->ports()) {
-      foreach (Connection* connection, port->connections()) {
-        if (checkedConnections.contains(connection)) {
-          continue;
-        }
-        checkedConnections << connection;
-
-        ConnectionInfo* connectionInfo = new ConnectionInfo();
-        connectionInfo->m_connection = connection;
-
-        if (connection->startPort()) {
-          connectionInfo->m_startPortNumber = connection->startPort()->number();
-          connectionInfo->m_startNodeName = connection->startPort()->parentNode()->name();
-        }
-        if (connection->endPort()) {
-          connectionInfo->m_endPortNumber = connection->endPort()->number();
-          connectionInfo->m_endNodeName = connection->endPort()->parentNode()->name();
-        }
-        if (connection->endConnector()) {
-          connectionInfo->m_endConnectorInfo.m_connector = connection->endConnector();
-          connectionInfo->m_endConnectorInfo.m_dstConnection = connection->endConnector()->dstConnection();
-        }
-
-        nodeRemoveInfo->m_connectionInfos << connectionInfo;
-      }
-    }
-    m_nodeRemoveInfos << nodeRemoveInfo;
-  }
+  m_nodes = nodes;
 }
 
 NodeRemoveCommand::~NodeRemoveCommand() {
 }
 
 void NodeRemoveCommand::redo() {
-  foreach (NodeRemoveInfo* nodeRemoveInfo, m_nodeRemoveInfos) {
-    m_sheet->removeNode(nodeRemoveInfo->m_node);
-    m_scene->removeNode(nodeRemoveInfo->m_node);
-    foreach (ConnectionInfo* connectionInfo, nodeRemoveInfo->m_connectionInfos) {
-      m_sheet->removeConnection(connectionInfo->m_connection);
-      m_scene->removeConnection(connectionInfo->m_connection);
+  foreach (AbstractNode* node, m_nodes) {
+    m_sheet->removeNode(node);
+    m_scene->removeNode(node);
+  }
+  foreach (AbstractNode* node, m_nodes) {
+    foreach (Port* port, node->ports()) {
+      foreach (Connection* connection, port->connections()) {
+        if (m_connections.contains(connection)) {
+          continue;
+        }
+        // connection which start or end node is not removed, need to disconnect from remain node.
+        // both side node removed, unneed to disconnect because when undo/redo reconnect.
+        if (isStartNodeRemoved(connection) && !isEndNodeRemoved(connection)) {
+          Port* port = connection->endPort();
+          port->removeConnection(connection);
+          connection->removeEndPort();
+          ConnectionInfo* connectionInfo = new ConnectionInfo(connection, nullptr, port);
+          m_connectionInfos << connectionInfo;
+        } else if (!isStartNodeRemoved(connection) && isEndNodeRemoved(connection)) {
+          Port* port = connection->startPort();
+          port->removeConnection(connection);
+          connection->removeStartPort();
+          ConnectionInfo* connectionInfo = new ConnectionInfo(connection, port, nullptr);
+          m_connectionInfos << connectionInfo;
+        }
+        m_connections << connection;
+        m_sheet->removeConnection(connection);
+        m_scene->removeConnection(connection);
+      }
     }
   }
 }
 
 void NodeRemoveCommand::undo() {
-  // when connect distination node do not exist, connection cannot create
-  // so first loop create all nodes
-  foreach (NodeRemoveInfo* nodeRemoveInfo, m_nodeRemoveInfos) { m_scene->addNode(nodeRemoveInfo->m_node); }
-
-  foreach (NodeRemoveInfo* nodeRemoveInfo, m_nodeRemoveInfos) {
-    foreach (ConnectionInfo* connectionInfo, nodeRemoveInfo->m_connectionInfos) {
-      if (connectionInfo->m_endConnectorInfo.m_connector) {
-        connectionInfo->m_endConnectorInfo.m_dstConnection->addBranchConnector(
-            connectionInfo->m_endConnectorInfo.m_connector);
-      }
-    }
+  foreach (AbstractNode* node, m_nodes) {
+    m_sheet->addNode(node);
+    m_scene->addNode(node);
   }
+  foreach (Connection* connection, m_connections) {
+    m_sheet->addConnection(connection);
+    m_scene->addConnection(connection);
+  }
+  foreach (ConnectionInfo* connectionInfo, m_connectionInfos) { connectionInfo->reconnect(); }
+}
+
+bool NodeRemoveCommand::isStartNodeRemoved(const Connection* connection) const {
+  AbstractNode* startNode = connection->startPort()->parentNode();
+  return m_nodes.contains(startNode);
+}
+
+bool NodeRemoveCommand::isEndNodeRemoved(const Connection* connection) const {
+  AbstractNode* endNode = connection->endPort()->parentNode();
+  return m_nodes.contains(endNode);
 }
